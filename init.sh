b7375689a78027c238d419be7d33c86db31c3e4a
Francisco Miamoto <shigueo.miamoto@gmail.com>
2020-01-05 02:07:37 -0300

    feat: add descriptive messages to init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# Install dependencies
+echo "
+Installing dependencies...
+sudo is needed for installing dependencies globally
+
+"
 sudo npm install -g @commitlint/cli @commitlint/config-conventional
 
-# Configure git's hooks dir
+echo "
+Configuring git hooks directory...
+"
 git config core.hooksPath .githooks
